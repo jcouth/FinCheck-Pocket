6ec76387c3444ed5663c10b03df3898006b384ee
jCouth <craftcouth@gmail.com>
2025-06-20 21:14:45 -0300

    chore: add Storybook configuration

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// const SHOW_STORYBOOK = process.env.SHOW_STORYBOOK === 'true';
+
+// const _App = SHOW_STORYBOOK ? StoryBookApp : App;
+
 AppRegistry.registerComponent(appName, () => App);
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
 /**
  * Metro configuration
@@ -8,4 +10,20 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const finalConfig = mergeConfig(getDefaultConfig(__dirname), config);
+
+module.exports = withStorybook(finalConfig, {
+  // Set to false to remove storybook specific options
+  // you can also use a env variable to set this
+  enabled: true,
+  // Path to your storybook config
+  configPath: path.resolve(__dirname, './.rnstorybook'),
+  // note that this is the default so you can the config path blank if you use .rnstorybook
+
+  // Optional websockets configuration
+  // Starts a websocket server on the specified port and host on metro start
+  // websockets: {
+  //   port: 7007,
+  //   host: 'localhost',
+  // },
+});
