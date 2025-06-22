3e75cf4bdf2ba69ab80735e1e426c88905c32d5b
jCouth <craftcouth@gmail.com>
2025-06-22 00:30:06 -0300

    chore: add .env configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'FINCHECK_POCKET_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: true,
+        allowUndefined: false,
+      },
+    ],
+  ],
 };
