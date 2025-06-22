c3f50fd5fbebcd0661dd73e2495bcd28adba246f
jCouth <craftcouth@gmail.com>
2025-06-22 04:25:00 -0300

    chore: add react-native-gesture-handler

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,18 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NewAppScreen templateFileName="App.tsx" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
